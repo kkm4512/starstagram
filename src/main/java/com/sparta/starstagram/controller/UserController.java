6140d8d0e99913754933c06f97741da218aa1220
kkm4512 <nayoun440@naver.com>
2024-09-02 19:14:23 +0900

    feat. 전부 병합 성공

--- src/main/java/com/sparta/starstagram/controller/UserController.java
+++ src/main/java/com/sparta/starstagram/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.sparta.starstagram.controller;
 
-import com.sparta.starstagram.repository.UserRepository;
 import com.sparta.starstagram.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
