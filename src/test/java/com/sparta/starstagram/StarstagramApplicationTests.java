3993d3d5848df391d1f9414f28880232fd43d8aa
kkm4512 <nayoun440@naver.com>
2024-09-02 12:56:47 +0900

    init

---
+++ src/test/java/com/sparta/starstagram/StarstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.starstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
